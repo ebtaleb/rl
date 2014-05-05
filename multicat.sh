3e20d24d05fa05d1faa937485000ae167db368c0
EB <elias.boutaleb@etu.upmc.fr>
2014-05-05 20:11:53 +0200

    tmux panes numbers are 1-indexed

--- multicat.sh
+++ multicat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 tmux new -A -s 'main' -n 'servers' -d
-tmux send-keys -t main:0.0 "cat playerpipe" Enter
-tmux splitw -v -t main:0.0
-tmux send-keys -t main:0.1 "cat itempipe" Enter
+tmux send-keys -t main:1.1 "cat playerpipe" Enter
+tmux splitw -v -t main:1.1
+tmux send-keys -t main:1.2 "cat itempipe" Enter
 tmux attach-session
 
